0eca39e32bfb6b0b858d65422e540ec12e4ef9d2
sarbajit Bhattacharjee <webdev@ranktechsolutions.com>
2022-08-30 19:23:20 +0530

    Update RgilRestController.java

--- src/main/java/com/rank/rgil/controller/RgilRestController.java
+++ src/main/java/com/rank/rgil/controller/RgilRestController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.rank.rgil.response.AgentLoginResponse;
 import com.rank.rgil.response.CustomerDto;
 import com.rank.rgil.rest.service.CCMSRESTControllerService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/rest")
 public class RgilRestController {
